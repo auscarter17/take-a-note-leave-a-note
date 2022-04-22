006a0f60020f22cef908152f08d5172cb8f526d2
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-04-22 02:50:23 -0400

    fixed api routing, adding notes is fully functional

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,1 +1,16 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
+const db = require('../../db/db')
+const { notes } = require('../../db/db');
+const { findById, createNewNote } = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+    res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+    const newNote = createNewNote(req.body, db);
+    res.json(newNote);
+});
+
+module.exports = router;
